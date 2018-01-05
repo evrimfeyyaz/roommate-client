5469347d915383352096770b449008aa816bbbdc
Evrim P <evrimfeyyaz@gmail.com>
2018-01-05 21:40:43 +0300

    Remove unused folder, rename controls folder

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export { default as ItemCard } from './Cards/ItemCard/ItemCard'
 export { default as BackgroundCard } from './Cards/BackgroundCard/BackgroundCard'
-export { default as Switch } from './Controls/Switch/Switch'
+export { default as Switch } from './controls/Switch/Switch'
 export { default as SvgIcon } from './Miscellaneous/SvgIcon/SvgIcon'
 export { default as Heading } from './Typography/Heading/Heading'
 export { default as Heading2 } from './Typography/Heading2/Heading2'
@@ -8,10 +8,10 @@ export { default as Heading3 } from './Typography/Heading3/Heading3'
 export { default as Title } from './Typography/Title/Title'
 
 // Controls
-export { default as Stepper } from './Controls/Stepper'
-export { default as CircularButton } from './Controls/CircularButton'
-export { default as SecondaryButton } from './Controls/SecondaryButton'
-export { default as PrimaryButton } from './Controls/PrimaryButton'
+export { default as Stepper } from './controls/Stepper'
+export { default as CircularButton } from './controls/CircularButton'
+export { default as SecondaryButton } from './controls/SecondaryButton'
+export { default as PrimaryButton } from './controls/PrimaryButton'
 
 // Navigation
 export { default as SideMenuItem } from './Navigation/SideMenu/Item/SideMenuItem'
