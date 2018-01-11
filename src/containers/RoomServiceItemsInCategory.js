97f9e6aa2555894d15dcea27aa73a057a99b3941
Evrim P <evrimfeyyaz@gmail.com>
2018-01-11 16:39:54 +0300

    Update Apollo, move redux connection from the room service screen to the items in category container

--- src/containers/RoomServiceItemsInCategory.js
+++ src/containers/RoomServiceItemsInCategory.js
@@ -1,16 +1,95 @@
 // @flow
 import React from 'react'
-import { ActivityIndicator } from 'react-native'
+import { ActivityIndicator, StyleSheet, View } from 'react-native'
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
-import type { OperationComponent } from 'react-apollo'
+import { connect } from 'react-redux'
+import Modal from 'react-native-modal'
 
-import { ItemsInCategory } from '../components'
+import { ItemsInCategory, ItemDetails } from '../components'
+import * as Redux from '../redux/roomServiceScreen'
 import type { ShoppingCategory, ShoppingItem } from '../types/shopping'
+import type { ApolloDataObject } from '../types/apollo'
+
+type DispatchProps = {
+  showRoomServiceItem: (item: ShoppingItem) => void,
+  hideRoomServiceItem: () => void
+}
+
+type Props = {
+  data?: {
+    ...ApolloDataObject,
+    roomServiceCategory: ShoppingCategory
+  }
+} & Redux.State & DispatchProps
+
+const RoomServiceItemsInCategory = (props: Props) => {
+  // Means query was skipped.
+  if (props.data == null) {
+    return null
+  }
+
+  const {
+    data,
+    showRoomServiceItem,
+    hideRoomServiceItem,
+    isSelectedRoomServiceItemVisible,
+    selectedRoomServiceItem
+  } = props
+  const { loading, roomServiceCategory } = data
+
+  // TODO: Use a global activity indicator.
+  if (loading) {
+    return <ActivityIndicator />
+  }
+
+  return (
+    <View>
+      <ItemsInCategory items={roomServiceCategory.items} onItemPress={showRoomServiceItem} />
+
+      <Modal
+        isVisible={isSelectedRoomServiceItemVisible}
+        animationInTiming={150}
+        animationOutTiming={150}
+        backdropTransitionInTiming={150}
+        backdropTransitionOutTiming={150}
+        onBackdropPress={hideRoomServiceItem}
+        backdropColor="#131519"
+        backdropOpacity={0.9}
+        style={styles.modal}
+      >
+        <ItemDetails
+          item={selectedRoomServiceItem}
+          onCloseButtonPress={hideRoomServiceItem}
+          style={styles.itemDetails}
+        />
+      </Modal>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  modal: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  itemDetails: {
+    width: '85%'
+  }
+})
+
+const mapStateToProps = state => ({
+  ...state.roomServiceScreen
+})
+
+const mapDispatchToProps = dispatch => ({
+  showRoomServiceItem: item => dispatch(Redux.showRoomServiceItem(item)),
+  hideRoomServiceItem: () => dispatch(Redux.hideRoomServiceItem())
+})
 
 const getRoomServiceCategoryWithItems = gql`
-  query getRoomServiceCategoryWithItems($categoryId: ID!) {
-    roomServiceCategory(id: $categoryId) {
+  query getRoomServiceCategoryWithItems($selectedRoomServiceCategoryId: ID!) {
+    roomServiceCategory(id: $selectedRoomServiceCategoryId) {
       id
       items {
         id
@@ -22,22 +101,8 @@ const getRoomServiceCategoryWithItems = gql`
   }
 `
 
-type Response = {
-  roomServiceCategory: ShoppingCategory
-}
+const graphQlContainer = graphql(getRoomServiceCategoryWithItems, {
+  skip: ownProps => ownProps.selectedRoomServiceCategoryId == null
+})(RoomServiceItemsInCategory)
 
-type InputProps = {
-  categoryId: string,
-  onItemPress: (ShoppingItem) => void
-}
-
-const withRoomServiceItems: OperationComponent<Response, InputProps> = graphql(getRoomServiceCategoryWithItems)
-
-export default withRoomServiceItems(({ data: { roomServiceCategory, loading }, onItemPress }) => {
-  // TODO: Use a global activity indicator.
-  if (loading) {
-    return <ActivityIndicator />
-  }
-
-  return <ItemsInCategory items={roomServiceCategory.items} onItemPress={onItemPress} />
-})
+export default connect(mapStateToProps, mapDispatchToProps)(graphQlContainer)
